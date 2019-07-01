8739c2290c101c83d3eb92dcd3c3fd9085266c49
longle <1125702468@qq.com>
2019-07-01 20:40:52 +0800

    修改图标

--- app/src/main/java/com/yolocc/eyepetizerunofficial/MainActivity.kt
+++ app/src/main/java/com/yolocc/eyepetizerunofficial/MainActivity.kt
@@ -7,7 +7,6 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
 import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -21,11 +20,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                toolbar.title = getString(R.string.title_dashboard)
+                toolbar.title = getString(R.string.title_discover)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                toolbar.title = getString(R.string.title_notifications)
+                toolbar.title = getString(R.string.title_ranking)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -57,22 +56,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_setting_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
